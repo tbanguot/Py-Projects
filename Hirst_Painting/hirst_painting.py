efc6bf28ce67d01c4687582aa838c03d79ebb20c
Tongol Banguot <tongolmalual@gmail.com>
2025-11-05 21:46:53 -0400

    Added output image

--- Hirst_Painting/hirst_painting.py
+++ Hirst_Painting/hirst_painting.py
@@ -6,7 +6,7 @@ t.colormode(255)
 turtle = t.Turtle()
 
 # List of rgb colors
-rgb_colors =  [
+COLOR_PALETTE =  [[255, 0, 0], [0, 255, 0], [0, 0, 255],
     (230, 209, 82), (56, 79, 137), (220, 129, 162), (227, 151, 80), (119, 179, 214), (169, 45, 76),
     (217, 56, 100), (173, 159, 24), (233, 79, 48), (135, 219, 179), (10, 61, 149), (124, 61, 54),
     (176, 26, 61), (143, 209, 229), (107, 49, 40), (65, 132, 214), (32, 159, 214), (158, 184, 230),
@@ -18,18 +18,22 @@ rgb_colors =  [
 pos_x = -200
 pos_y = -200
 
+DOT_SIZE = 22
+GRID_SIZE = 10
+SPACING = 50
+
 turtle.penup()
 
 # Draw hirst painting
-for _ in range(10):
-    for _ in range(10):
+for _ in range(GRID_SIZE):
+    for _ in range(GRID_SIZE):
         turtle.setposition(pos_x, pos_y)
-        color = random.choice(rgb_colors)
-        turtle.dot(22, color)
-        pos_x += 50
+        color = random.choice(COLOR_PALETTE)
+        turtle.dot(DOT_SIZE, color)
+        pos_x += SPACING
 
     pos_x = -200
-    pos_y += 50
+    pos_y += SPACING
     turtle.speed(0)
     turtle.goto(pos_x, pos_y)
 
